97cba30bdf8c98d550ef3beacd4c22f8223e397d
Arun Deepak Akunuri <68249451+Arun200207@users.noreply.github.com>
2022-03-23 15:06:14 +0530

    Add files via upload

--- GUI_data_Project.py
+++ GUI_data_Project.py
@@ -1,10 +1,30 @@
 from tkinter import * 
 from tkinter import messagebox
 #import CSV_Read
-#import CSV_Read1
+
+def work():
+    file=file_value.get()
+    import pandas as pd
+
+
+    file=str(input())
+
+    df = pd.read_csv(file)
+
+    print(df.to_string())
 
 
 window=Tk()
 window.title('DATA ANALYTICS ')
 window.geometry('1000x500')
+lab=Label(window,text="ENTER THE FILE:",fg='black')
+lab.place(x=100,y=50)
+file_value=Entry(window,width=250)
+file_value.place(x=100,y=100)
+
+button=Button(window,text="ENTER",fg="blue",command=work)
+button.pack()
+
+
+
 window.mainloop()
