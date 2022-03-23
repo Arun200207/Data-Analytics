ebfe2895e726e62c00d3174496b5a7385b6bf9d9
Arun Deepak Akunuri <68249451+Arun200207@users.noreply.github.com>
2022-03-23 14:43:34 +0530

    Add files via upload

---
+++ GUI_data_Project.py
@@ -0,0 +1,10 @@
+from tkinter import * 
+from tkinter import messagebox
+#import CSV_Read
+#import CSV_Read1
+
+
+window=Tk()
+window.title('DATA ANALYTICS ')
+window.geometry('1000x500')
+window.mainloop()
